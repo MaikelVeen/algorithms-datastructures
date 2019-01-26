c7d8d8bf2a325539ea2c6200dface978c8510038
Maikel Veen <veenmaikel@gmail.com>
2019-01-26 21:05:51 +0100

    Add partial implementation of simple weighted graph
    - Add tests for initialization

---
+++ Data Structures/Graph.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data_Structures
+{
+    /// <summary>
+    /// Data structure implementation for a weighted simple graph
+    /// </summary>
+    public class Graph<T> where T : IComparable
+    {
+        /// <summary>
+        /// Creates an empty graph
+        /// </summary>
+        public Graph()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Creates an graph with vertices based on vertex data
+        /// but with no edges
+        /// </summary>
+        /// <param name="vertexData"></param>
+        public Graph(T value)
+        {
+            AddVertex(value, Count);
+        }
+        
+        /// <summary>
+        /// Creates an graph with vertices based on vertex data
+        /// but with no edges
+        /// </summary>
+        /// <param name="vertexData"></param>
+        public Graph(T[] values)
+        {
+            InitValues(values);
+        }
+
+        /// <summary>
+        /// Creates a graph with vertices and edges based on given
+        /// vertex data and edges
+        /// Tuple must contain valid indexes for vertices
+        /// </summary>
+        /// <param name="vertexData"></param>
+        /// <param name="edges"></param>
+        public Graph(T[] values, Tuple<int, int>[] edges)
+        {
+            InitValues(values);
+            foreach (Tuple<int,int> edge in edges)
+            {
+                SetEdge(edge);
+            }
+        }
+        
+        // Extracted helper function because it is used in multiple constructors
+        private void InitValues(T[] values)
+        {
+            foreach (T value in values)
+            {
+                AddVertex(value, Count, false);
+            }
+            ResizeAdjacencyMatrix();
+        }
+
+        private List<Vertex<T>> Vertices { get; set; } = new List<Vertex<T>>();
+        public int[,] AdjancencyMatrix { get; private set; }
+        public int Count { get; private set; } 
+        
+        /// <summary>
+        /// Adds a new vertex to the graph, returns the index of the added vertex
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public int AddVertex(T data)
+        {
+            int index = Count;
+            AddVertex(data, index);
+            return index;
+        }
+
+        private void AddVertex(T data,int index, bool automaticResize = true)
+        {
+            Count++;
+            Vertex<T> vertex = new Vertex<T>(index, data);
+            Vertices.Add(vertex);
+            if(automaticResize) {ResizeAdjacencyMatrix();}        
+        }
+        
+        /// <summary>
+        /// Adds an edge between two indexed vertices with a given weight to the graph.
+        /// Indexes must exist.
+        /// Default weight is 1.
+        /// </summary>
+        /// <param name="edge">Tuple that describes from which index to which index</param>
+        /// <param name="weight"></param>
+        public void SetEdge(Tuple<int,int> edge, int weight=1)
+        {
+            if (edge.Item1 > Count - 1 || edge.Item2 > Count - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+            AdjancencyMatrix[edge.Item1, edge.Item2] = weight;
+            AdjancencyMatrix[edge.Item2, edge.Item1] = weight;
+        }
+        
+        /// <summary>
+        /// Returns the weight of edge between two vertexes
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public int GetEdgeWeight(int from, int to)
+        {
+            if (from > Count - 1 || to > Count - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return AdjancencyMatrix[from, to];
+        }
+        
+        /// <summary>
+        /// Resizes the adjancency matrix after an vertex is added
+        /// </summary>
+        private void ResizeAdjacencyMatrix()
+        {
+            if (AdjancencyMatrix != null && AdjancencyMatrix.GetLength(0) != Count)
+            {
+                int[,] newMatrix = new int[Count,Count];
+
+                for (int x = 0; x < AdjancencyMatrix.GetLength(0); x++)
+                {
+                    for (int y = 0; y < AdjancencyMatrix.GetLength(0); y++)
+                    {
+                        newMatrix[x, y] = AdjancencyMatrix[x, y];
+                    }
+                }
+
+                AdjancencyMatrix = newMatrix;
+            }
+            else
+            {
+                AdjancencyMatrix = new int[Count,Count];
+            }
+        }
+
+        public string BreadthFirstTraversal(StringBuilder stringBuilder)
+        {
+            Queue<int> queue = new Queue<int>();
+            throw  new NotImplementedException();
+                
+        }
+        
+        public string BreadFirstTraversal(StringBuilder stringBuilder)
+        {
+            throw  new NotImplementedException();
+                
+        }
+
+        public Vertex<T> AtIndex(int index)
+        {
+            return Vertices.Find(x => x.Index.Equals(index));
+        }
+    }
+
+    public class Vertex<T> 
+
+    {
+        public Vertex(int index, T data)
+        {
+            Index = index;
+            Data = data;
+        }
+
+        public int Index { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ Testing/GraphTests.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Text;
+using Data_Structures;
+using Xunit;
+
+namespace Testing
+{
+    public class GraphTests
+    {
+        private Graph<string> graph;
+
+        private readonly string[] graphData = new[] {"A", "B", "C", "D", "E", "F", "G"};
+
+        private readonly Tuple<int, int>[] edges = new[]
+        {
+            new Tuple<int, int>(0,1),
+            new Tuple<int, int>(1,2),
+            new Tuple<int, int>(2,3),
+            new Tuple<int, int>(0,4),
+            new Tuple<int, int>(4,1),
+            new Tuple<int, int>(1,5),
+            new Tuple<int, int>(5,2),
+            new Tuple<int, int>(2,6),
+            new Tuple<int, int>(6,3)
+
+        };
+        
+        private readonly string[] graphDijkstra = new[] {"A", "B", "C", "D", "E", "F"};
+        
+        private const string BreadthTraversal = "ABECFDG";
+        private const string DepthTraversal = "ABCDGFE";
+
+        public GraphTests()
+        {
+            graph = new Graph<string>();
+        }
+
+        #region Initialization Tests
+
+        [Fact]
+        public void InitialSizeZero()
+        {
+            Assert.Equal(0, graph.Count);
+        }
+
+        [Fact]
+        public void InitWithSingleVertex()
+        {
+            graph = new Graph<string>("A");
+            Assert.Equal(1, graph.Count);
+            Assert.Equal("A", graph.AtIndex(0).Data);
+        }
+
+        [Fact]
+        public void InitWithVertices()
+        {
+            graph = new Graph<string>(new []{"A","B","C"});
+            Assert.Equal(3, graph.Count);
+            Assert.Equal("A", graph.AtIndex(0).Data);
+            Assert.Equal("B", graph.AtIndex(1).Data);
+            Assert.Equal("C", graph.AtIndex(2).Data);
+            
+            Assert.Equal(3, graph.AdjancencyMatrix.GetLength(0));
+            Assert.Equal(3, graph.AdjancencyMatrix.GetLength(1));
+        }
+        
+        [Fact]
+        public void InitWithVerticesAndEdges()
+        {
+            graph = new Graph<string>(graphData,edges);
+            Assert.Equal(7, graph.Count);
+            Assert.Equal("A", graph.AtIndex(0).Data);
+            Assert.Equal("G", graph.AtIndex(6).Data);
+            
+            Assert.Equal(1,graph.GetEdgeWeight(0,1));
+            // Should be only one direction, so value should be same
+            Assert.Equal(1,graph.GetEdgeWeight(1,0));
+            
+            Assert.Equal(1,graph.GetEdgeWeight(1,2));
+            Assert.Equal(1,graph.GetEdgeWeight(2,3));
+            
+            Assert.Equal(1,graph.GetEdgeWeight(0,4));
+            Assert.Equal(1,graph.GetEdgeWeight(4,1));
+            Assert.Equal(1,graph.GetEdgeWeight(1,5));
+            
+            Assert.Equal(1,graph.GetEdgeWeight(5,2));
+            Assert.Equal(1,graph.GetEdgeWeight(2,6));
+            Assert.Equal(1,graph.GetEdgeWeight(6,3));
+        }
+
+        #endregion
+
+        #region Adding Tests
+
+        // TODO add vertex and edge adding tests
+
+        #endregion
+
+        #region Traversal Tests
+
+        
+
+        #endregion
+
+        #region Dijkstra Tests
+
+        
+
+        #endregion
+
+        #region Floyd Warschall Tests
+
+        
+
+        #endregion
+    }
+}
